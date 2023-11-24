1be01aafcaf8268c863387f335659c324178ccb7
Christina Wilfing <christina.wilfing@outlook.com>
2023-11-24 13:36:02 +0100

    21 -- refactoring

--- src/react-query/hooks/useAddTodos.ts
+++ src/react-query/hooks/useAddTodos.ts
@@ -24,11 +24,13 @@ const useAddTodo = (onAdd: () => void) => {
 
       return { previousTodos };
     },
+
     onSuccess: (savedTodo, newTodo) => {
       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
         todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
       );
     },
+
     onError: (error, newTodo, context) => {
       if (!context) return;
       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos);
