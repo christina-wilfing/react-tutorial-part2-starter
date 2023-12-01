b5db49bb5614c6aba3b8870f240aae546d11df6a
Christina Wilfing <christina.wilfing@outlook.com>
2023-12-01 16:06:26 +0100

    6 - nested Routes

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,10 @@
+import { log } from "console";
+import { useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log(useSearchParams());
+
   return <p>User</p>;
 };
 
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,19 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/users",
-    element: <UserListPage />,
-  },
-  {
-    path: "/users/:id",
-    element: <UserDetailPage />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "users",
+        element: <UserListPage />,
+      },
+      {
+        path: "users/:id",
+        element: <UserDetailPage />,
+      },
+    ],
   },
 ]);
 
