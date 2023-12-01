8f6fe8769c45e24c4f53a13a7c7a206a2ebdd407
Christina Wilfing <christina.wilfing@outlook.com>
2023-12-01 14:59:31 +0100

    Revert "2 - setting up Routing"
    This reverts commit fb60f2f525c73538d2b88f0a8e042d63fc8ca3f5.

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
-import router from "./routing/routes";
-import { RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <App />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
